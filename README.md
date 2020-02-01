577db714474d490c5db5d4e736802107968029a9
ykmc <34961813+ykmc@users.noreply.github.com>
2020-02-01 17:25:30 +0900

    atcoder client tools v0.1.5

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 - python 3.8.0
 - pipenv
 
+# how to use
+
+```sh
+pipenv install git+https://github.com/ykmc/atcoder-client-tools.git@<release>#egg=atcoder-client-tools
+```
+release は v0.0.0的なやつ
+
--- src/atcoder/cli.py
+++ src/atcoder/cli.py
@@ -1,12 +1,26 @@
 import click
+import subprocess
+import pkgutil
+from atcoder.chrome import Chrome
 
 @click.group()
 def main():
     pass
 
 @main.command()
-def test():
-    print("test")
+@click.argument("name")
+def test(name):
+    #chrome = Chrome()
+    #chrome.get()
+
+    args = ["python"]
+    args.append(name)
+    #with open('x.txt', encoding='UTF-8') as fp:
+    #    cp = subprocess.run(args, stdin=fp)
+
+    input_str = pkgutil.get_data("atcoder", "data/abc153_a_in1.txt")
+    cp = subprocess.run(args, input=input_str)
+
 
 if __name__ == "__main__":
     main()
