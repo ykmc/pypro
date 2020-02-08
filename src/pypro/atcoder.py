bf524edec35893a20f4f682d2c814095bc2a142c
ykmc <34961813+ykmc@users.noreply.github.com>
2020-02-08 20:53:46 +0900

    [wip] feat: pp atcoder test

--- src/pypro/atcoder.py
+++ src/pypro/atcoder.py
@@ -1,40 +1,60 @@
 import click
+import sys
+import os
+import subprocess
+import pkgutil
 from pathlib import Path
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+# ----------------------------------------------------------------
+
 @click.group()
 def atcoder():
     pass
 
+# --------------------------------
+# pp atcoder get ...
+# --------------------------------
 @atcoder.command()
-@click.argument("url")
+@click.argument("url_or_contest")
 @click.option("--problem", "-p", default="all")
-def get(url, problem):
+def get(url_or_contest, problem):
     atcoder = AtCoder()
     # contest が URLでない場合, URLに整形する
-    if not atcoder.is_url(url):
-        url = "https://atcoder.jp/contests/" + url
+    url = atcoder.get_atcoder_url(url_or_contest)
     # テストケースを取得
     data = atcoder.get(Path(url), problem)
     # テストケースを格納
     atcoder.save(data)
 
+# --------------------------------
+# pp atcoder init ...
+# --------------------------------
 @atcoder.command()
 def init():
     print("init")
     # ファイルが既にあるならエラー
     # ない場合、作成する
 
+# --------------------------------
+# pp atcoder test ...
+# --------------------------------
 @atcoder.command()
-@click.pass_context
-def test(ctx):
-    print("test")
-    # ケースがローカルにない場合、getする
-    #ctx.forward(get, problem="a")
+@click.argument("python_file")
+def test(python_file):
+    atcoder = AtCoder()
+    # テストの準備
+    contest,problem,commands = atcoder.prepare_test(python_file)
+    # テストケースを取得
+    testcase_list = atcoder.get_testcase_files(contest, problem)
     # テスト実行
+    atcoder.exec_test(testcase_list, contest, problem, commands)
 
+# --------------------------------
+# pp atcoder git ...
+# --------------------------------
 @atcoder.command()
 def git():
     print("git")
@@ -44,6 +64,8 @@ def git():
     # git add filepath
     # git commit -m "xxx"
     # git push origin master
+
+# ----------------------------------------------------------------
 
 class AtCoder:
 
@@ -58,7 +80,13 @@ class AtCoder:
             return True
         else:
             return False
-    
+
+    def get_atcoder_url(self, string):
+        if string[0:19] == "https://atcoder.jp/":
+            return string
+        else:
+            return "https://atcoder.jp/contests/" + string
+
     def get(self, url, problems):
         contest = str(url).split("/")[-1]
         contest_dir_path = self.data_dir_path / contest
@@ -131,6 +159,41 @@ class AtCoder:
             else:
                 with path.open(mode='w') as f:
                     f.write(data.string)
+    
+    def prepare_test(self, python_file):
+        name = python_file.replace(".py","")
+        contest = name.split("_")[0]
+        problem = name.split("_")[1]
+        commands = ["python"]
+        commands.append(name + ".py")
+        return (contest,problem,commands)
+    
+    def get_testcase_files(self, contest, problem):
+        test_case_dir_path = self.data_dir_path / contest / problem
+        return sorted(test_case_dir_path.glob("in_*.txt"))
+    
+    def exec_test(self, testcase_list, contest, problem, commands):
+        # ケースがローカルにない場合
+        if len(testcase_list) == 0:
+            print("テストケースを事前にダウンロードしてください")
+            sys.exit()
+        # テスト実行
+        test_case_dir = "data/" + contest + "/" + problem
+        for testcase in testcase_list:
+            input_data  = pkgutil.get_data("pypro", test_case_dir + "/" + testcase.name)
+            output_data = pkgutil.get_data("pypro", test_case_dir + "/" + (testcase.name).replace("in", "out")) # あとでリファクタする
+            res = subprocess.run(commands, input=input_data, check=True, stdout=subprocess.PIPE)
+            
+            status = "AC" if output_data==res.stdout else "WA"
+            print(testcase.name + " : " + status)
+            print("[input]")
+            print(input_data.decode().rstrip('\n'))
+            print("[expected]")
+            print(output_data.decode().rstrip('\n'))
+            print("[your answer]")
+            print(res.stdout.decode().rstrip('\n'))
+            print("")
+
 
 if __name__ == "__main__":
     atcoder()
--- src/pypro/cli.py
+++ src/pypro/cli.py
@@ -1,13 +1,14 @@
 import click
 from pypro.atcoder import atcoder
 
-
 @click.group()
 def main():
     pass
 
+# --------------------------------
+# pp atcoder xxx
+# --------------------------------
 main.add_command(atcoder)
-
 
 if __name__ == "__main__":
     main()
